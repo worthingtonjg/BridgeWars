1bf4e2fbe297d2e9a1b5a34a2a014a7199d4e86e
Jon Worthington <worthingtonjg@gmail.com>
2022-10-15 20:25:52 -0600

    adding key controls

--- Assets/Reconnect/LevelController.cs
+++ Assets/Reconnect/LevelController.cs
@@ -8,6 +8,12 @@ public class LevelController : MonoBehaviour
 {
     public string nextScene;
     public GameObject canvas;
+    public int rows;
+    public int cols;
+    public string PlayerName;
+
+    private int selectedRow;
+    private int selectedCol;
 
     private List<GameObject> connectors;
     private List<Connector> connectorComponents = new List<Connector>();
@@ -15,18 +21,26 @@ public class LevelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var connectors = GameObject.FindGameObjectsWithTag("Connector").ToList();
+        var connectors = GameObject.FindGameObjectsWithTag("Connector").OrderBy(c => c.name).ToList();
         connectors.ForEach(c => connectorComponents.Add(c.GetComponent<Connector>()));
+        connectorComponents[0].ToggleSelected(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print($"Solved: {connectorComponents.Count(c => c.Solved())}");
-
         if(connectorComponents.All(c => c.Solved()))
         {
             StartCoroutine("EnableNextLevel");
+        }
+
+        if(PlayerName=="P1")
+        {
+            if(Input.GetKeyDown(KeyCode.A)) MoveLeft();
+            if(Input.GetKeyDown(KeyCode.D)) MoveRight();
+            if(Input.GetKeyDown(KeyCode.W)) MoveUp();
+            if(Input.GetKeyDown(KeyCode.S)) MoveDown();
+            if(Input.GetKeyDown(KeyCode.Space)) Rotate();
         }
     }
 
@@ -45,5 +59,63 @@ public class LevelController : MonoBehaviour
         {
             SceneManager.LoadScene(nextScene);
         }
+    }
+
+    private void MoveLeft()
+    {
+        --selectedCol;
+        if(selectedCol < 0)
+        {
+            selectedCol = cols - 1;
+        }
+        UpdateSelected();
+    }
+
+    private void MoveRight()
+    {
+        ++selectedCol;
+        if(selectedCol >= cols)
+        {
+            selectedCol = 0;
+        }                
+        UpdateSelected();
+    }
+
+    private void MoveDown()
+    {
+        ++selectedRow;
+        if(selectedRow >= rows)
+        {
+            selectedRow = 0;
+        }
+        UpdateSelected();
+    }
+
+    private void MoveUp()
+    {
+        --selectedRow;
+        if(selectedRow < 0)
+        {
+            selectedRow = rows - 1;
+        }
+
+        UpdateSelected();
+    }
+
+    private void UpdateSelected()
+    {
+        connectorComponents.ForEach(c => c.ToggleSelected(false));
+        GetSelected().ToggleSelected(true);
+    }
+
+    private Connector GetSelected()
+    {
+        var index = selectedCol + (selectedRow * cols);        
+        return connectorComponents[index];
+    }
+
+    private void Rotate()
+    {
+        GetSelected().Rotate();
     }
 }
